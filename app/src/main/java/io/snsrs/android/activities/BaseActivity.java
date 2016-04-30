6807769d3508bf770217f96e2c380c83b4ac1bb4
Segun Famisa <segun.famisa@konga-Famisas-MBP.local>
2016-04-30 17:43:57 +0100

    Init commit

---
+++ app/src/main/java/io/snsrs/android/activities/BaseActivity.java
@@ -0,0 +1,10 @@
+package io.snsrs.android.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by segun.famisa on 30/04/2016.
+ */
+public class BaseActivity extends AppCompatActivity {
+
+}
---
+++ app/src/main/java/io/snsrs/android/fragments/BaseFragment.java
@@ -0,0 +1,9 @@
+package io.snsrs.android.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by segun.famisa on 30/04/2016.
+ */
+public class BaseFragment extends Fragment {
+}
